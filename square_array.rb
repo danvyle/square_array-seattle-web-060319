6103f362287b60857f3cd8a604a399066d47c84b
Dan-Vy Le <danvyle85@gmail.com>
2019-05-14 00:43:44 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,5 +1,15 @@
 def square_array(array)
-  each.do |i|
-    new.array << i ** 2
-    return new.array
+  squared = []
+  array.each.do |i|
+  squared << (i ** 2)
+  end 
+    return squared
 end
+
+# def square_array(numbers)
+#   new_numbers = []
+# end 	  numbers.each do |num|
+#     new_numbers << (num ** 2)
+#   end
+#   return new_numbers
+# end
